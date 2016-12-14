8702c384d2ee6afcccb8d724e31d8e5c0359ce62
Debra Gordon <debra.m.gordon@gmail.com>
2016-12-13 18:31:08 -0600

    getting profile/auth working, close

--- javascripts/controllers/JobDetailCtrl.js
+++ javascripts/controllers/JobDetailCtrl.js
@@ -1,6 +1,6 @@
 "use strict";
 
-app.controller("JobDetailCtrl", function ($scope, $routeParams, JobFactory){
+app.controller("JobDetailCtrl", function ($scope, $routeParams, JobFactory, InterestFactory, UserFactory){
 
   $scope.selectedJob = {};
 
@@ -9,6 +9,15 @@ app.controller("JobDetailCtrl", function ($scope, $routeParams, JobFactory){
 
   JobFactory.getSingleJob(jobId).then(function(oneJob){
     oneJob.id = jobId;
+    $scope.selectedJob.users = [];
+    InterestFactory.getInterests(jobId).then(function(interestedPeople){
+      let interesedPeopleArray = interestedPeople;
+      interesedPeopleArray.forEach(function(person){
+        UserFactory.getUser(person.uid).then(function(userInfo){
+          $scope.selectedJob.users.push(userInfo);
+        });
+      });
+    });
     console.log("oneJob", oneJob);
     $scope.selectedJob = oneJob;
   });
---
+++ javascripts/factories/InterestFactory.js
@@ -0,0 +1,8 @@
+"use strict";
+
+app.factory("InterestFactory", function($q, $http, FIREBASE_CONFIG) {
+
+
+
+    return{};
+});
