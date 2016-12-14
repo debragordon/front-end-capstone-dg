19068de431d30ac5edb34a4b82ced2c89720651c
Debra Gordon <debra.m.gordon@gmail.com>
2016-12-14 13:16:10 -0600

    interest logic working

--- javascripts/controllers/MyJobsCtrl.js
+++ javascripts/controllers/MyJobsCtrl.js
@@ -1,7 +1,23 @@
 "use strict";
 
-app.controller("MyJobsCtrl", function ($scope, $routeParams, JobFactory){
+app.controller("MyJobsCtrl", function ($scope, $routeParams, $rootScope, JobFactory, InterestFactory){
 
-  console.log("route connected");
+    let currentUser = $rootScope.user.uid;
+    $scope.interestedJobs = [];
 
+    InterestFactory.getInterestsByUser(currentUser).then(function(response){
+      let interestedPeopleArray = response;
+      interestedPeopleArray.forEach(function(person){
+        JobFactory.getSingleJob(person.jobId).then(function(jobDetails){
+          jobDetails.interestId = person.id;
+          $scope.interestedJobs.push(jobDetails);
+        });
+      });
+    });
+
+    $scope.removeInterest = function(job){
+    InterestFactory.deleteInterest(job.interestId).then(function(){
+        $scope.interestedJobs.splice($scope.interestedJobs.indexOf(job),1);
+    });
+  };
 });
